c08c7e8f1e14d37e8c594ee599fa96470a53f13e
FareedKandalaft <fareed@spectrecoder.com>
2021-04-05 15:55:50 -0400

    Completed project implemented with components. State is managed through prop drilling techinques

--- src/App.js
+++ src/App.js
@@ -13,6 +13,7 @@ class App extends Component {
   state = {
     users: [],
     user: {},
+    repos: [],
     loading: false,
     alert: null,
   };
@@ -24,6 +25,16 @@ class App extends Component {
   //   );
   //   this.setState({ users: res.data.items, loading: false });
   // }
+
+  // get users repos
+  getUserRepos = async (username) => {
+    this.setState({ loading: true });
+
+    const res = await axios.get(
+      `https://api.github.com/users/${username}/repos?per_page=5&sort=created&direction=desc&client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`
+    );
+    this.setState({ repos: res.data, loading: false });
+  };
 
   // not clear if its necessary to have param have same name as prop
   searchUsers = async (search) => {
@@ -50,7 +61,7 @@ class App extends Component {
   };
 
   render() {
-    const { users, user, loading } = this.state;
+    const { users, user, repos, loading } = this.state;
 
     return (
       <Router>
@@ -84,7 +95,9 @@ class App extends Component {
                   <User
                     {...props}
                     getUser={this.getUser}
+                    getUserRepos={this.getUserRepos}
                     user={user}
+                    repos={repos}
                     loading={loading}
                   />
                 )}
