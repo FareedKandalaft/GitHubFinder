ab68305e4536d3e2b96920fefb61c8517d4a6a27
FareedKandalaft <fareed@spectrecoder.com>
2021-04-18 16:33:33 -0400

    Home and NotFound components created and implemented

--- src/App.js
+++ src/App.js
@@ -1,14 +1,13 @@
-import React, { Fragment } from 'react';
+import React from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import Navbar from './components/layout/Navbar';
 import Alert from './components/layout/Alert';
-import Users from './components/users/Users';
 import User from './components/users/User';
-import Search from './components/users/Search';
 import About from './components/pages/About';
-
 import GithubState from './context/github/GithubState';
 import AlertState from './context/alert/AlertState';
+import Home from './components/pages/Home';
+import NotFound from './components/pages/NotFound';
 import './App.css';
 
 const App = () => {
@@ -21,21 +20,12 @@ const App = () => {
             <div className='container'>
               <Alert />
               <Switch>
-                <Route
-                  exact
-                  path='/'
-                  render={(props) => (
-                    <Fragment>
-                      <Search />
-                      {/* <Users loading={loading} users={users} /> */}
-                      <Users />
-                    </Fragment>
-                  )}
-                />
+                <Route exact path='/' component={Home} />
                 <Route exact path='/about' component={About} />
                 {/* Like Search the User will require state and data to be passed in */
                 /* so you need to use the render= methodology of Route */}
                 <Route exact path='/user/:login' component={User} />
+                <Route component={NotFound} />
               </Switch>
             </div>
           </div>
